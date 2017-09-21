d001789b81395ce830748aaf800edd7b93c46a37
yifang he <yifanghe@yifangs-MacBook-Pro.local>
2017-09-20 21:39:31 -0400

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by yifang he on 9/20/17.
+//  Copyright © 2017 yifang he. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
